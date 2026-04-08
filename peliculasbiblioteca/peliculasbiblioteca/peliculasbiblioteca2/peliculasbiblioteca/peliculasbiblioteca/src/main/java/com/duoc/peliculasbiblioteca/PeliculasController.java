d88a82b097ceb303f30700d295ae2f0f61dfadff
Gina Raffaela <gina.gaggero@gmail.com>
2026-04-08 15:46:57 -0400

    Se agrega operacion
    Se agrega addPelicula para agregar peliculas a la lista

---
+++ peliculasbiblioteca/peliculasbiblioteca/peliculasbiblioteca2/peliculasbiblioteca/peliculasbiblioteca/src/main/java/com/duoc/peliculasbiblioteca/PeliculasController.java
@@ -0,0 +1,42 @@
+package com.duoc.peliculasbiblioteca;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.duoc.peliculasbiblioteca.service.PeliculasService;
+import com.duoc.peliculasbiblioteca.model.Pelicula;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+public class PeliculasController {
+
+    @Autowired
+    private PeliculasService peliculasService;
+
+    //Metodo para agregar peliculas nuevas
+    @PostMapping
+    public Pelicula addPelicula(@RequestBody Pelicula entity) {
+        return peliculasService.createPelicula(entity);
+    }
+
+    //Metodo para obtencion de lista completa de peliculas
+    @GetMapping("/peliculas")
+    public List<Pelicula> getPeliculas() {
+        return peliculasService.getAllPeliculas();
+    }
+    
+    //Metodo de obtencion de pelicula segun el id entregado
+    @GetMapping("/peliculas/{id}")
+    public Optional<Pelicula> getPeliculasById(@PathVariable Long id) throws Exception {
+        return peliculasService.getPeliculaById(id);
+    }
+}
---
+++ peliculasbiblioteca/peliculasbiblioteca/peliculasbiblioteca2/peliculasbiblioteca/peliculasbiblioteca/src/main/java/com/duoc/peliculasbiblioteca/service/PeliculasService.java
@@ -0,0 +1,12 @@
+package com.duoc.peliculasbiblioteca.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.duoc.peliculasbiblioteca.model.Pelicula;
+
+public interface PeliculasService {
+    List<Pelicula> getAllPeliculas();
+    Optional<Pelicula> getPeliculaById(Long id);
+    Pelicula createPelicula(Pelicula pelicula);
+}
